3db1c04dd081ebaa26623766e8d3359b1cee8009
Grant Steinfeld <g@agentidea.com>
2011-02-02 18:25:37 -0500

    __init__.py added

---
+++ testAll.py
@@ -0,0 +1,10 @@
+from src.config import info as settings
+from src.framework.core import Utils as ute
+
+
+    
+    
+if __name__ == '__main__':
+    print "version %s " % str(settings.version)
+    print settings.appName
+    print "identity %s " % str(settings.identity)
